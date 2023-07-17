a460017d23147eee228f5653f7cd5e5bdb3cc01e
studentbcw <139743509+studentbcw@users.noreply.github.com>
2023-07-17 15:23:08 -0600

    update

--- script.js
+++ script.js
@@ -10,8 +10,6 @@
 590
 100000000000000000000000000000000000000000
 
-
-
 true
 false
 
@@ -31,3 +29,53 @@ let myNothingVariable = null;
 let greeting = "Hows it going?"
 
 console.log("Hello!" + myName + greeting)
+
+//print stuff to the consle
+//consle.log("hello " + myName + ". " + greetings)
+//consle.log(myAge, mygrade, doesntWearGlasses)
+//consle.log("supercalifragilisticexpialidocious".length)
+
+//we are bird wathing
+
+let ducks = 7;
+let geese = 2;
+let robins = 14;
+
+let totalBirds = 7 + 2 + 14;
+
+console.log("This is the total birds we watched i saw", totalBirds)
+
+let subtractionAnswer = 789 - 355;
+let multiplyer = 55 * 90;
+let divitionAnswer = 100 / 10;
+let modulerAnsewer = 9 % 2;
+
+//the modulus operator refers to reminders after doing devition.
+
+
+// control flows
+
+if(3 > 2) {
+console.log("three is grater than two!")
+}
+
+if (wearsGlasses) {
+    console.log ("I see and look actually good!")
+} else{
+    console.log("i see por")
+}
+
+// for loop
+
+let steps = 0;
+
+while(steps < 4) {
+    console.log("move forwatd");
+    steps++;
+}
+
+
+
+
+
+
