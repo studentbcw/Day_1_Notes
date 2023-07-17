6bedb1bb5e0394777ebba99349b8aae7ee47a79a
studentbcw <139743509+studentbcw@users.noreply.github.com>
2023-07-17 14:45:56 -0600

    init commit

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 // 🧩What is data? The answers to questions
 
+//there are three basic data types in javascript
+"hello there"
+"go!"
+"eli"
+"an entire scentance or two. i like strings! woo hoo!"
+
+4
+590
+100000000000000000000000000000000000000000
 
 
+
+true
+false
+
+let myName  = "Elijah";
+let myAge = 11
+let myGrade = 6
+let  wearsGlasses = "false"
+
+let total;
+//undefined
+
+let myNothingVariable = null;
+
+//truthy values: true, everything else.
+
+// falsy values: undefined, false, null, 0.
+let greeting = "Hows it going?"
+
+console.log("Hello!" + myName + greeting)
